ebe00c64ab829243011b0e98a2d8920f6f9ac47e
Lunat12 <nati12n@gmail.com>
2021-12-03 12:29:09 +0100

    bot1_v0.1

---
+++ bot1.py
@@ -0,0 +1,44 @@
+import random
+from matplotlib import pyplot as plt
+
+
+def graf(listN):
+    listD = []
+    date = 0
+    for n in listN:
+        date += 1
+        listD.append(date)
+    plt.plot(listD,listN)
+    plt.show
+
+
+print('WELCOME TO THE BASIC TRADING BOT')
+
+currencies = ['Bitcoin', 'Ada', 'Ethereum', 'Solana']
+
+currency = input('Select a currency to work with: ').title()
+
+try:
+    money = float(input('How much money do you want to invest: '))
+
+    H_resistance = float(input('Wheres the highest point you wanna be taking your money out (percentage): '))
+    L_resistance = float(input('Wheres the lowest point you wanna be taking your money out (percentage):'))
+except:
+    print('You can only enter numbers.')
+
+H_percent = money * (H_resistance / 100)
+L_percent =  -money * (L_resistance / 100)
+
+print(H_percent,' ', L_percent)
+
+points = []
+
+isActive = True
+while isActive:
+    num = random.randint(-50, 500)
+    points.append(num)
+    print(num)
+    if num >= H_percent or num <= L_percent:
+        isActive = False
+
+graf(points)
